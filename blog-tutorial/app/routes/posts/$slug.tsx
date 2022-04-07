ef13052877fe9de312d00fdbf0aa3c2aa35b57c2
okada genya <g.okada.wilf+github@gmail.com>
2022-04-07 23:20:43 +0900

    fix: markdownからhtmlの生成と表示

--- blog-tutorial/app/routes/posts/$slug.tsx
+++ blog-tutorial/app/routes/posts/$slug.tsx
@@ -3,27 +3,31 @@ import { useLoaderData} from '@remix-run/react'
 import type {LoaderFunction} from '@remix-run/node'
 import {getPost} from '~/models/post.server'
 import invariant from 'tiny-invariant'
+import {marked} from 'marked'
 
 import type {Post} from '~/models/post.server'
 
-type LoaderData = {post: Post}
+type LoaderData = {post: Post, html: string}
 
 export const loader: LoaderFunction = async ({params}) => {
   invariant(params.slug, `params.slug is required`)
   
   const post = await getPost(params.slug)
   invariant(post, `post not found: ${params.slug}`)
-  return json<LoaderData>({post})
+
+  const html = marked(post.markdown)
+  return json<LoaderData>({post, html})
 }
 
 
 export default function PostSlug() {
-  const {post} = useLoaderData() as LoaderData
+  const {post, html} = useLoaderData() as LoaderData
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">
         Some Post: {post.title}
       </h1>
+      <div dangerouslySetInnerHTML={{__html: html}} />
     </main>
   )
 }
