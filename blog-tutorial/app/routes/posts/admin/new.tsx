0003528fec91173146450d72b9c871f6efb8a708
okada genya <g.okada.wilf+github@gmail.com>
2022-04-23 21:45:32 +0900

    add: transition

--- blog-tutorial/app/routes/posts/admin/new.tsx
+++ blog-tutorial/app/routes/posts/admin/new.tsx
@@ -1,5 +1,5 @@
 import { redirect, json } from '@remix-run/node'
-import { Form, useActionData } from '@remix-run/react'
+import { Form, useActionData, useTransition } from '@remix-run/react'
 
 import { createPost } from '~/models/post.server'
 import type { ActionFunction } from '@remix-run/node'
@@ -14,12 +14,14 @@ type ActionData =
   | undefined
 
 export const action: ActionFunction = async ({ request }) => {
+  // TODO: remove me
+  await new Promise((res) => setTimeout(res, 1000))
+
   const formData = await request.formData()
 
   const title = formData.get('title')
   const slug = formData.get('slug')
   const markdown = formData.get('markdown')
-  console.log('aaaa')
 
   const errors: ActionData = {
     title: title ? null : 'Title is required',
@@ -54,6 +56,10 @@ const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
 
 export default function NewPost() {
   const errors = useActionData()
+
+  const transition = useTransition()
+  const isCreating = Boolean(transition.submission)
+
   return (
     <Form method="post">
       <p>
@@ -93,8 +99,9 @@ export default function NewPost() {
         <button
           type="submit"
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
         >
-          Create Post
+          {isCreating ? 'Creating...' : 'Create Post'}
         </button>
       </p>
     </Form>
